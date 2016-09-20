77fcd10e6003616e0dbf21257816a10b54e88cb7
Daniele Bertella <daniele.bertella@link-me.it>
2016-09-20 08:17:51 +0200

    add order functionality idea in actions test

--- src/actions/actions.test.js
+++ src/actions/actions.test.js
@@ -69,4 +69,22 @@ describe('Action creators', () => {
     store.dispatch(actions.sortArtistList('age', 'ASC'));
     expect(store.getActions()).toEqual(expectedActions);
   });
+
+  const crazyOrder = (values) => {
+    const median = Math.floor((values[0] + values[values.length - 1]) / 2);
+    return values
+      .slice(0, values.length - 1)
+      .reduce((p, n, i) => (
+        i % 2 === 0 ? p.concat(p[p.length - 1] + i + 1) : p.concat(p[p.length - 1] - i - 1)
+      ), [median]);
+  }
+
+  it('should do the crazy order with even records', () => {
+    const b = crazyOrder([25, 26, 27, 28, 29, 30]);
+    expect(b).toEqual([27, 28, 26, 29, 25, 30]);
+  });
+  it('should do the crazy order with odd records', () => {
+    const b = crazyOrder([25, 26, 27, 28, 29, 30, 31]);
+    expect(b).toEqual([28, 29, 27, 30, 26, 31, 25]);
+  });
 });
